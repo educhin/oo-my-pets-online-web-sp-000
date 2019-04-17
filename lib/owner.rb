4d3f7fd98b76a2d29b61af1a153dc93b5bbf41a5
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-17 01:30:02 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -60,6 +60,10 @@ class Owner
     end
   end
 
+  def sell_pets
+
+  end
+
   def list_pets
     "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)"
   end
