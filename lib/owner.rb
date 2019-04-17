a5bfa42f489761fe6b62d866d9b692312d9dc599
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-17 01:01:24 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,42 @@
 class Owner
-  # code goes here
+
+  attr_reader :species
+  attr_accessor :pets, :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = species
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def say_species
+    self.species
+  end
+
+  def buy_dog(name)
+    Dog.new(name)
+  end
+
+  def buy_cat(name)
+    Cat.new(name)
+  end
+
+  def buy_fish(name)
+    Fish.new(name)
+  end
 end
